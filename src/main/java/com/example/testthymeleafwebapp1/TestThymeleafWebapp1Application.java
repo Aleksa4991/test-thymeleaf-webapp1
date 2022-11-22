6328aecec5a22915681e62556bab25daf1d6a948
Aleksa4991 <aleksa4991@mail.ru>
2022-11-22 17:35:04 +0500

    Initial commit

---
+++ src/main/java/com/example/testthymeleafwebapp1/TestThymeleafWebapp1Application.java
@@ -0,0 +1,13 @@
+package com.example.testthymeleafwebapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestThymeleafWebapp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestThymeleafWebapp1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testthymeleafwebapp1/TestThymeleafWebapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testthymeleafwebapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestThymeleafWebapp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
